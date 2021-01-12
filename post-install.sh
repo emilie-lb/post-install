f75fb14b39f1275c098d83dd2f978791f1db690f
emilie-lb <62951146+emilie-lb@users.noreply.github.com>
2021-01-12 22:12:29 +0100

    Add files via upload

---
+++ post-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Updating APT..."
+sudo apt-get update 
+clear
+echo "Installing base packages"
+sudo apt-get install --yes git git-extras 
+sudo apt-get instal python3-pip
+echo "Intalling dicord"
+sudo snap install discord --classic
+echo "Installing Visual Studio code"
+sudo snap install code --classic
+echo "Installing Microsoft Teams"
